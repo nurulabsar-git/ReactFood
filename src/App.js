706e61e3eb841f38b8ef10f23c0edc2b1d509cb5
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-02-26 00:02:01 +0600

    update

--- src/App.js
+++ src/App.js
@@ -9,33 +9,51 @@ const App = () =>{
   
 //   const example = `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${App_key}`;
  const [recipes, setRecipes] = useState([]);
+ const [search, setSearch] = useState("");
+ const [query, setQuery] = useState('chicken');
 
 useEffect( () => {
    getRecipes();
 //  console.log('hello bangladesh');
-}, []);
+}, [query]);
 
 const getRecipes = async () =>{
-  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${App_key}`);
+  // const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${App_key}`);
+  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${App_key}`);
   const data = await response.json();
   // console.log(data.hits);
   setRecipes(data.hits);
   console.log(data.hits);
   console.log(data);
 
+};
+
+const updateSearch = e => {
+  setSearch(e.target.value);
+  console.log(search);
+};
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
 }
 
-  return(
-    <div className ="App">
-      <form className ="search-form">
-        <input className ="search-bar" type="text"/>
+return(
+<div className ="App">
+      <form onSubmit={getSearch} className ="search-form">
+        <input value ={search} onChange={updateSearch} className ="search-bar" type="text"/>
         <button className ="search-button" type="submit">Search</button>
       </form>
-      {recipes.map(recipeA =>(
-     <Recipe title ={recipeA.recipe.label} calories ={recipeA.recipe.calories}
-      images = {recipeA.recipe.image}></Recipe>
+     <div className ="recipes"> 
+    {recipes.map(recipeA =>(
+     <Recipe key ={recipeA.recipe.label}
+      title ={recipeA.recipe.label} calories ={recipeA.recipe.calories}
+      images = {recipeA.recipe.image} ingredientsA ={recipeA.recipe.ingredients}>
+      
+      </Recipe>
   ))}
-    </div>
+  </div>
+</div>
   );
 };
 
